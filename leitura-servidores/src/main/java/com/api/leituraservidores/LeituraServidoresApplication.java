f214b8c82404054e93eaa4724af136ec9145bb97
Davi-Hilario <davi.hilario@sptech.school>
2023-12-15 17:30:20 -0300

    criando projeto java com as dependências do spring

---
+++ leitura-servidores/src/main/java/com/api/leituraservidores/LeituraServidoresApplication.java
@@ -0,0 +1,13 @@
+package com.api.leituraservidores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeituraServidoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeituraServidoresApplication.class, args);
+	}
+
+}
